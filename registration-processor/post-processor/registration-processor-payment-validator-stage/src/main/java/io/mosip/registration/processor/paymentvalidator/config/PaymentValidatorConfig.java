0a8ae66382f55246bf87e8bae9db0c5a33a72e77
Ibrahim Nkambo <mnsglw@gmail.com>
2024-06-10 11:25:29 +0300

    oauth2 impl clean up on payment validate stage

--- registration-processor/post-processor/registration-processor-payment-validator-stage/src/main/java/io/mosip/registration/processor/paymentvalidator/config/PaymentValidatorConfig.java
+++ registration-processor/post-processor/registration-processor-payment-validator-stage/src/main/java/io/mosip/registration/processor/paymentvalidator/config/PaymentValidatorConfig.java
@@ -3,6 +3,8 @@ package io.mosip.registration.processor.paymentvalidator.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.web.client.RestTemplate;
 
 import io.mosip.registration.processor.paymentvalidator.util.CustomizedRestApiClient;
