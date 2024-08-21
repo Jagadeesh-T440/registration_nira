6f53c0626c24b3a80001ce6e0c615cb267b83dca
Jagadeesh T <jagadeesh.t@technoforte.co.in>
2024-08-21 13:57:10 +0530

    Nira

---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/constant/MappingJsonConstants.java
@@ -0,0 +1,89 @@
+package io.mosip.registration.processor.core.constant;
+
+public class MappingJsonConstants {
+	public static final String VALUE = "value";
+	public static final String POA = "poa";
+	public static final String POB = "pob";
+	public static final String POR = "por";
+	public static final String POI = "poi";
+	public static final String POE = "poe";
+	public static final String INDIVIDUAL_BIOMETRICS = "individualBiometrics";
+	public static final String AUTHENTICATION_BIOMETRICS = "individualAuthBiometrics";
+	public static final String INTRODUCER_UIN = "introducerUIN";
+	public static final String INTRODUCER_RID = "introducerRID";
+	public static final String IDENTITY = "identity";
+	public static final String METAINFO = "metaInfo";
+	public static final String AUDITS = "audits";
+	public static final String VALIDATE = "validate";
+	public static final String DOCUMENT = "documents";
+	public static final String BIOMETRICS = "biometrics";
+	public static final String PREFERRED_LANGUAGE = "preferredLanguage";
+	public static final String CONSENT="consent";
+
+	public static final String INTRODUCER_BIO = "introducerBiometrics";
+	/** The Constant OFFICERBIOMETRICFILENAME. */
+	public static final String OFFICERBIOMETRICFILENAME = "officerBiometricFileName";
+
+	/** The Constant SUPERVISORBIOMETRICFILENAME. */
+	public static final String SUPERVISORBIOMETRICFILENAME = "supervisorBiometricFileName";
+	/** Supervisor Authentication. */
+	public static final String SUPERVISOROTPAUTHENTICATION = "supervisorOTPAuthentication";
+	public static final String EMAIL = "email";
+	public static final String PHONE = "phone";
+	public static final String ADDRESS = "address";
+	public static final String NAME = "name";
+	public static final String DOB = "dob";
+	public static final String GENDER = "gender";
+	public static final String NRCID = "nrcId";
+	public static final String SELECTED_HANDLES = "selectedHandles";	public static final String TYPE = "type";
+	public static final String FORMAT = "format";
+	public static final String AGE = "age";
+	public static final String IDSCHEMA_VERSION = "IDSchemaVersion";
+	public static final String UIN = "uin";
+	public static final String LOCATION_HIERARCHY_FOR_PROFILING = "locationHierarchyForProfiling";
+	
+	
+/* Citizenship Verification */
+public static final String APPLICANT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+public static final String APPLICANT_CLAN = "applicantPlaceOfOriginClan";
+public static final String APPLICANT_PLACE_OF_ORIGIN = "applicantPlaceOfOriginDistrict";
+public static final String FATHER_NIN = "fatherNIN";
+public static final String FATHER_TRIBE = "fatherIndigenousCommunityTribe";
+public static final String FATHER_CLAN = "fatherIndigenousCommunityClan";
+public static final String PARENT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+public static final String PARENT_CLAN = "applicantPlaceOfOriginClan";
+public static final String PARENT_PLACE_OF_ORIGIN = "applicantPlaceOfOriginDistrict";
+public static final String FATHER_PLACE_OF_ORIGIN = "fatherPlaceOfOriginDistrict";
+public static final String FATHER_SURNAME = "fatherSurname";
+public static final String FATHER_GIVENNAME = "fatherGiveName";
+public static final String FATHER_OTHERNAMES = "fatherOtherNames";
+public static final String MOTHER_NIN = "motherNIN";
+public static final String MOTHER_TRIBE = "motherIndigenousCommunityTribe";
+public static final String MOTHER_CLAN = "motherClan";
+public static final String MOTHER_PLACE_OF_ORIGIN = "motherPlaceOfResidenceDistrict";
+public static final String MOTHER_SURNAME = "motherSurname";
+public static final String MOTHER_GIVENNAME = "motherGiveName";
+public static final String MOTHER_OTHERNAMES = "motherOtherNames";
+public static final String GUARDIAN_NIN = "guardianNIN_AIN";
+public static final String GUARDIAN_RELATION_TO_APPLICANT = "guardianRelationToApplicant";
+public static final String GUARDIAN_CLAN = "applicantPlaceOfOriginClan";
+public static final String GUARDIAN_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+
+public static final String GUARDIAN_PLACE_OF_ORIGIN = "applicantplaceoforigin";
+public static final String APPLICANT_DATEOFBIRTH = "dateOfBirth";
+public static final String APPLICANT_CITIZENSHIPTYPE = "applicantCitizenshipType";
+public static final String FATHER_LIVINGSTATUS = "fatherlivingStatus";
+public static final String MOTHER_LIVINGSTATUS = "motherlivingstatus";
+public static final String CLAN = "Clan";
+public static final String TRIBE = "Tribe";
+public static final String PERSON = "person";
+public static final String TRIBE_ON_FORM ="Tribe";
+public static final String CLAN_ON_FORM="Clan";
+public static final String PLACE_OF_ORIGIN = "PlaceOfOriginDistrict";
+public static final String GUARDIAN_TRIBE_FORM = "guardianTribe";
+public static final String GUARDIAN_CLAN_FORM = "guardianClan";
+public static final String GUARDIAN_LIVING_STATUS = "guardianlivingstatus";
+public static final String DATE_FORMAT = "yyyy/MM/dd";
+public static final String NIN_STATUS = "status";
+public static final String DISABILITY = "disabilities";
+}
